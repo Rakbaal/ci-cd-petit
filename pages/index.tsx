20f3d6dd857aedeb8ad6f2a27ad1c053cbfa8b2d
Leo <leo.carpentier@overspeed.fr>
2023-06-14 16:22:11 +0200

    dsbnzerklg

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Production
+          ALLO
         </h1>
 
         <p className={styles.description}>
